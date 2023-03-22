a70ac30bff8750e2247a7c44d2671ea5c5cb48f3
angieyth <angie@aptoslabs.com>
2023-03-22 16:26:51 -0700

    [ANS][TS SDK] add minting name function

--- ecosystem/typescript/sdk/src/plugins/ans_client.ts
+++ ecosystem/typescript/sdk/src/plugins/ans_client.ts
@@ -1,8 +1,8 @@
-import { Provider } from "../providers/";
+import { AptosClient, Provider } from "../providers";
 import * as Gen from "../generated/index";
-import {AptosAccount} from "../account";
-import {AnyNumber} from "../bcs";
-import {TransactionBuilderRemoteABI} from "../transaction_builder";
+import { AptosAccount } from "../account";
+import { AnyNumber } from "../bcs";
+import { TransactionBuilderRemoteABI } from "../transaction_builder";
 
 const ansContractsMap: Record<string, string> = {
   testnet: "0x5f8fd2347449685cf41d4db97926ec3a096eaf381332be4f1318ad4d16a8497c",
@@ -37,7 +37,6 @@ type NameRegistryV1 = {
 
 export class AnsClient {
   contractAddress: string;
-  provider: Provider;
 
   provider: Provider;
 
@@ -106,31 +105,23 @@ export class AnsClient {
    * @param years year duration of the domain name
    * @returns The hash of the transaction submitted to the API
    */
-  async mintAptosName(
-      account: AptosAccount,
-      domainName: string,
-      years: AnyNumber = 1,
-  ): Promise<string> {
-      // check if the name is valid
-      if (domainName.match(nameComponentPattern) === null) {
-        throw new Error(`Name ${domainName} is not valid`);
-      }
-      // check if the name is available
-      const address = await this.getAddressByName(domainName);
-      if (address !== null) {
-          throw new Error(`Name ${domainName} is not available`);
-      }
+  async mintAptosName(account: AptosAccount, domainName: string, years: AnyNumber = 1): Promise<string> {
+    // check if the name is valid
+    if (domainName.match(nameComponentPattern) === null) {
+      throw new Error(`Name ${domainName} is not valid`);
+    }
+    // check if the name is available
+    const address = await this.getAddressByName(domainName);
+    if (address !== null) {
+      throw new Error(`Name ${domainName} is not available`);
+    }
 
-      const builder = new TransactionBuilderRemoteABI(this.provider.aptosClient, { sender: account.address()});
-      const rawTxn = await builder.build(
-          `${this.contractAddress}::domains::register_domain`,
-          [],
-          [domainName, years],
-      );
+    const builder = new TransactionBuilderRemoteABI(this.provider.aptosClient, { sender: account.address() });
+    const rawTxn = await builder.build(`${this.contractAddress}::domains::register_domain`, [], [domainName, years]);
 
-      const bcsTxn = AptosClient.generateBCSTransaction(account, rawTxn);
-      const pendingTransaction = await this.provider.aptosClient.submitSignedBCSTransaction(bcsTxn);
-      return pendingTransaction.hash;
+    const bcsTxn = AptosClient.generateBCSTransaction(account, rawTxn);
+    const pendingTransaction = await this.provider.aptosClient.submitSignedBCSTransaction(bcsTxn);
+    return pendingTransaction.hash;
   }
 
   /**
